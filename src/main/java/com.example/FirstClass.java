dd619bf9fe2e4597a72f33b98b0d4879eb68d676
anirudhreddy <anirudhreddy020@gmail.com>
2023-06-03 12:58:34 -0500

    New class added

---
+++ src/main/java/com.example/FirstClass.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import com.example.OOPS.Vehicle;
+
+import static com.example.OOPS.Vehicle.start;
+
+public class FirstClass {
+
+    public static void main(String[] args)  {
+        Vehicle car = new Vehicle();
+        car.start();
+        car.stop();
+
+    }
+
+}
